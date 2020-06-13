9b70765a83382bd1efd7d640d7ce31b1332efc51
roykimm <roykimmyunghoon@gmail.com>
2020-06-14 00:42:50 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.get('/',(req,res) => {
+    res.send('hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`서버가 시작되었습니다. (${PORT} 포트)`)
+});
+
