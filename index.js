ee3955dcd349e4ba78fbe5c82a6e6451eeb2ebb4
roykimm <roykimmyunghoon@gmail.com>
2020-06-14 17:56:04 +0900

    test4

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 7000;
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 app.get('/',(req,res) => {
-    res.send('hi');
+    res.sendFile(path.join(__dirname,"client/build","index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`서버가 시작되었습니다. (${PORT} 포트)`)
+    console.log(`${PORT} 포트에서 서버가 시작되었습니다.`)
 });
 
